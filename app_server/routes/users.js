44e5fcb6e770a7581ebfc2394e8ed18150bcad0d
Luis Miguel Banda Montes <bandalui@hpe.com>
2016-04-16 14:41:50 -0500

    added local and social media authentication using OAuth to the project

--- app_server/routes/users.js
+++ app_server/routes/users.js
@@ -1,9 +1,33 @@
+/**
+ * API keys and Passport configuration.
+ */
+var passportConfig = require('../config/passport');
+
 var express = require('express');
-var router = express.Router();
+var app = express.Router();
+
+var userController = require('../controllers/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-});
+});*/
 
-module.exports = router;
+app.get('/login', userController.getLogin);
+app.post('/login', userController.postLogin);
+app.get('/logout', userController.logout);
+app.get('/forgot', userController.getForgot);
+app.post('/forgot', userController.postForgot);
+app.get('/reset/:token', userController.getReset);
+app.post('/reset/:token', userController.postReset);
+app.get('/signup', userController.getSignup);
+app.post('/signup', userController.postSignup);
+/*app.get('/contact', contactController.getContact);
+app.post('/contact', contactController.postContact);*/
+app.get('/account', passportConfig.isAuthenticated, userController.getAccount);
+app.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
+app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
+app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
+app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
+
+module.exports = app;
