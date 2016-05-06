bd0f7df207907a0c2c71609e33d0d9994d7a8cd4
loko <binario200@gmail.com>
2016-05-06 10:43:58 -0500

    removing the user controller - route

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var bodyParser = require('body-parser');
 //var users = require('./routes/users');
 // change to do the app and MVC app
 var routes = require('./app_server/routes/index');
-var users = require('./app_server/routes/users');
 
 var app = express();
 
@@ -29,7 +28,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
